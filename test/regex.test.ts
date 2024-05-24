6e2c74f223355ca52024bd8cea26f003134a1425
lehuygiang28 <lehuygiang28@gmail.com>
2024-05-24 11:21:06 +0700

    refactor: improve regex tests

--- test/regex.test.ts
+++ test/regex.test.ts
@@ -9,7 +9,7 @@ describe('createRegex', () => {
 
     it('should generate a regular expression that matches the keyword with or without diacritical marks and with different variations of the same letter', () => {
         const keyword = 'Hà Nội';
-        const expectedRegex = new RegExp(`[H][à][ ][N][ộ][iíìỉĩị]`, 'i');
+        const expectedRegex = /[H][à][ ][N][ộ][iíìỉĩị]/i;
         expect(createRegex(keyword)).toEqual(expectedRegex);
     });
 
@@ -18,7 +18,7 @@ describe('createRegex', () => {
         const options: CreateRegexOptions = {
             sensitive: false,
         };
-        const expectedRegex = new RegExp(`[H][à][ ][N][ộ][iíìỉĩị]`, 'i');
+        const expectedRegex = /[H][à][ ][N][ộ][iíìỉĩị]/i;
         expect(createRegex(keyword, options)).toEqual(expectedRegex);
     });
 
@@ -27,63 +27,63 @@ describe('createRegex', () => {
         const options: CreateRegexOptions = {
             sensitive: true,
         };
-        const expectedRegex = new RegExp(`[H][à][ ][N][ộ][iíìỉĩị]`);
+        const expectedRegex = /[H][à][ ][N][ộ][iíìỉĩị]/;
         expect(createRegex(keyword, options)).toEqual(expectedRegex);
     });
 
     [
-        { input: 'Hà Nội', expected: '[H][à][ ][N][ộ][iíìỉĩị]' },
-        { input: 'hà nội', expected: '[h][à][ ][n][ộ][iíìỉĩị]' },
-        { input: 'HÀ NỘI', expected: '[H][À][ ][N][Ộ][IÍÌỈĨỊ]' },
-        { input: 'ha noi', expected: '[h][aáàảãạăắằẳẵặâấầẩẫậ][ ][n][oóòỏõọôốồổỗộơớờởỡợ][iíìỉĩị]' },
-        { input: 'HA NOI', expected: '[H][AÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ][ ][N][OÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ][IÍÌỈĨỊ]' },
+        { input: 'Hà Nội', expected: /[H][à][ ][N][ộ][iíìỉĩị]/ },
+        { input: 'hà nội', expected: /[h][à][ ][n][ộ][iíìỉĩị]/ },
+        { input: 'HÀ NỘI', expected: /[H][À][ ][N][Ộ][IÍÌỈĨỊ]/ },
+        { input: 'ha noi', expected: /[h][aáàảãạăắằẳẵặâấầẩẫậ][ ][n][oóòỏõọôốồổỗộơớờởỡợ][iíìỉĩị]/ },
+        { input: 'HA NOI', expected: /[H][AÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ][ ][N][OÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ][IÍÌỈĨỊ]/ },
     ].forEach(({ input, expected }) => {
         it(`should generate a regex with output case is same: ${input} - ${expected}`, () => {
             const options: CreateRegexOptions = {
                 sensitive: true,
             };
-            expect(createRegex(input, options)).toEqual(new RegExp(expected));
+            expect(createRegex(input, options)).toEqual(expected);
         });
     });
 
     [
-        { input: 'Hà Nội', expected: '[h][à][ ][n][ộ][iíìỉĩị]' },
-        { input: 'hà nội', expected: '[h][à][ ][n][ộ][iíìỉĩị]' },
-        { input: 'HÀ NỘI', expected: '[h][à][ ][n][ộ][iíìỉĩị]' },
-        { input: 'HA NOI', expected: '[h][aáàảãạăắằẳẵặâấầẩẫậ][ ][n][oóòỏõọôốồổỗộơớờởỡợ][iíìỉĩị]' },
+        { input: 'Hà Nội', expected: /[h][à][ ][n][ộ][iíìỉĩị]/ },
+        { input: 'hà nội', expected: /[h][à][ ][n][ộ][iíìỉĩị]/ },
+        { input: 'HÀ NỘI', expected: /[h][à][ ][n][ộ][iíìỉĩị]/ },
+        { input: 'HA NOI', expected: /[h][aáàảãạăắằẳẵặâấầẩẫậ][ ][n][oóòỏõọôốồổỗộơớờởỡợ][iíìỉĩị]/ },
     ].forEach(({ input, expected }) => {
         it(`should generate a regex with output case is lower: ${input} - ${expected}`, () => {
             const options: CreateRegexOptions = {
                 sensitive: true,
                 outputCase: 'lower',
             };
-            expect(createRegex(input, options)).toEqual(new RegExp(expected));
+            expect(createRegex(input, options)).toEqual(expected);
         });
     });
 
     [
-        { input: 'Hà Nội', expected: '[H][À][ ][N][Ộ][IÍÌỈĨỊ]' },
-        { input: 'hà nội', expected: '[H][À][ ][N][Ộ][IÍÌỈĨỊ]' },
-        { input: 'HÀ NỘI', expected: '[H][À][ ][N][Ộ][IÍÌỈĨỊ]' },
-        { input: 'HA NOI', expected: '[H][AÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ][ ][N][OÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ][IÍÌỈĨỊ]' },
+        { input: 'Hà Nội', expected: /[H][À][ ][N][Ộ][IÍÌỈĨỊ]/ },
+        { input: 'hà nội', expected: /[H][À][ ][N][Ộ][IÍÌỈĨỊ]/ },
+        { input: 'HÀ NỘI', expected: /[H][À][ ][N][Ộ][IÍÌỈĨỊ]/ },
+        { input: 'HA NOI', expected: /[H][AÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ][ ][N][OÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ][IÍÌỈĨỊ]/ },
     ].forEach(({ input, expected }) => {
         it(`should generate a regex with output case is UPPER: ${input} - ${expected}`, () => {
             const options: CreateRegexOptions = {
                 sensitive: true,
                 outputCase: 'upper',
             };
-            expect(createRegex(input, options)).toEqual(new RegExp(expected));
+            expect(createRegex(input, options)).toEqual(expected);
         });
     });
 
     [
-        { input: 'Hà Nội', expected: '[hH][àÀ][ ][nN][ộỘ][iíìỉĩịIÍÌỈĨỊ]' },
-        { input: 'hà nội', expected: '[hH][àÀ][ ][nN][ộỘ][iíìỉĩịIÍÌỈĨỊ]' },
-        { input: 'HÀ NỘI', expected: '[hH][àÀ][ ][nN][ộỘ][iíìỉĩịIÍÌỈĨỊ]' },
+        { input: 'Hà Nội', expected: /[hH][àÀ][ ][nN][ộỘ][iíìỉĩịIÍÌỈĨỊ]/ },
+        { input: 'hà nội', expected: /[hH][àÀ][ ][nN][ộỘ][iíìỉĩịIÍÌỈĨỊ]/ },
+        { input: 'HÀ NỘI', expected: /[hH][àÀ][ ][nN][ộỘ][iíìỉĩịIÍÌỈĨỊ]/ },
         {
             input: 'HA NOI',
             expected:
-                '[hH][aáàảãạăắằẳẵặâấầẩẫậAÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ][ ][nN][oóòỏõọôốồổỗộơớờởỡợOÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ][iíìỉĩịIÍÌỈĨỊ]',
+                /[hH][aáàảãạăắằẳẵặâấầẩẫậAÁÀẢÃẠĂẮẰẲẴẶÂẤẦẨẪẬ][ ][nN][oóòỏõọôốồổỗộơớờởỡợOÓÒỎÕỌÔỐỒỔỖỘƠỚỜỞỠỢ][iíìỉĩịIÍÌỈĨỊ]/,
         },
     ].forEach(({ input, expected }) => {
         it(`should generate a regex with output case is both lower and UPPER: ${input} - ${expected}`, () => {
@@ -91,7 +91,7 @@ describe('createRegex', () => {
                 sensitive: true,
                 outputCase: 'lowerAndUpper',
             };
-            expect(createRegex(input, options)).toEqual(new RegExp(expected));
+            expect(createRegex(input, options)).toEqual(expected);
         });
     });
 });
